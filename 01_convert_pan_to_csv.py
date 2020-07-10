f120e753a69f9c41a5f1341be83ce058fc4471f8
aeryen <aeryen@gmail.com>
2020-07-10 14:00:04 -0500

    clean up and trying new plots

--- 01_convert_pan_to_csv.py
+++ 01_convert_pan_to_csv.py
@@ -5,8 +5,9 @@ import textacy
 
 class PANData(object):
     re1 = re.compile(r'  +')
+    re2 = re.compile(r"(\\uf.{3})")
 
-    def __init__(self, year, train_split, test_split, known_as_list=False):
+    def __init__(self, year, train_split, test_split, known_as="list"):
         """
         this class go find the PAN dataset dir, check it, list it, load the whole txt.
         also read in truth.txt for labels.
@@ -32,8 +33,10 @@ class PANData(object):
             for problem_dir in dir_list[split_name]:
                 k_docs, u_doc = self.load_one_problem(problem_dir)
                 l = train_labels[os.path.basename(problem_dir)]
-                if known_as_list:
+                if known_as == "list":
                     self.train_splits.append({"label": l, 'k_doc': k_docs, 'u_doc': u_doc})
+                elif known_as == "str":
+                    self.train_splits.append({"label": l, 'k_doc': " ".join(k_docs), 'u_doc': u_doc})
                 else:
                     for k in k_docs:
                         self.train_splits.append({"label": l, 'k_doc': k, 'u_doc': u_doc})
@@ -52,8 +55,10 @@ class PANData(object):
             for problem_dir in dir_list[split_name]:
                 k_docs, u_doc = self.load_one_problem(problem_dir)
                 l = test_labels[os.path.basename(problem_dir)]
-                if known_as_list:
+                if known_as == "list":
                     self.test_splits.append({"label": l, 'k_doc': k_docs, 'u_doc': u_doc})
+                elif known_as == "str":
+                    self.test_splits.append({"label": l, 'k_doc': " ".join(k_docs), 'u_doc': u_doc})
                 else:
                     for k in k_docs:
                         self.test_splits.append({"label": l, 'k_doc': k, 'u_doc': u_doc})
@@ -86,6 +91,8 @@ class PANData(object):
 
     def fixup(self, x):
         x = x.replace("\n", " xcrlfx ")
+        # x = self.re2.sub(r" \1 ", x)
+        x = x.encode("ascii", "ignore").decode("utf-8") 
         # x = textacy.preprocess_text(x, fix_unicode=True, transliterate=True)
         # x = self.re1.sub(' ', x)
         return x
@@ -95,7 +102,7 @@ class PANData(object):
         k_docs = []
         u_doc = None
         for doc_file in doc_file_list:
-            with open(os.path.join(problem_dir, doc_file), encoding='utf-8') as f:
+            with open(os.path.join(problem_dir, doc_file), encoding='utf-8-sig') as f:
                 if doc_file.startswith("known"):
                     doc = f.read()
                     doc = self.fixup(doc)
@@ -117,166 +124,56 @@ class PANData(object):
         return self.test_splits
 
 
-def convert_all_lm():
-    PATH_LM = Path('data_new/pan_all_lm/')
-    PATH_LM.mkdir(exist_ok=True)
-
-    pan_data13 = PANData(year="13", train_split=["pan13_train"], test_split=["pan13_test01", "pan13_test02"])
-    pan_data14 = PANData(year="14", train_split=["pan14_train_english-novels", "pan14_train_english-essays"],
-                         test_split=["pan14_test01_english-essays", "pan14_test01_english-novels",
-                                     "pan14_test02_english-essays", "pan14_test02_english-novels"])
-    pan_data15 = PANData(year="15", train_split=["pan15_train"], test_split=["pan15_test"])
-
-    all_txt = pan_data13.get_train()['u_doc']
-    all_txt = all_txt.append(pan_data13.get_train()['k_doc'])
-
-    all_txt = all_txt.append(pan_data13.get_test()['u_doc'])
-    all_txt = all_txt.append(pan_data13.get_test()['k_doc'])
-
-    all_txt = all_txt.append(pan_data14.get_train()['u_doc'])
-    all_txt = all_txt.append(pan_data14.get_train()['k_doc'])
-
-    all_txt = all_txt.append(pan_data14.get_test()['u_doc'])
-    all_txt = all_txt.append(pan_data14.get_test()['k_doc'])
-
-    all_txt = all_txt.append(pan_data15.get_train()['u_doc'])
-    all_txt = all_txt.append(pan_data15.get_train()['k_doc'])
-
-    all_txt = all_txt.append(pan_data15.get_test()['u_doc'])
-    all_txt = all_txt.append(pan_data15.get_test()['k_doc'])
-
-    uniq_doc = pd.unique(all_txt.values.ravel('K'))
-
-    np.random.seed(42)
-    rdn_idx = np.random.permutation(len(uniq_doc))
-
-    uniq_doc = uniq_doc[rdn_idx]
-
-    with open(PATH_LM / 'train.txt', 'w', encoding="utf-8") as train_file:
-        for i in range(len(uniq_doc) - 80):
-            train_file.write(uniq_doc[i] + "\n")
-
-    with open(PATH_LM / 'test.txt', 'w', encoding="utf-8") as test_file:
-        for i in range(len(uniq_doc) - 80, len(uniq_doc)):
-            test_file.write(uniq_doc[i] + "\n")
-
-    print("ok")
-
-
-def convert_pan14_essay_lm():
-    PATH_LM = Path('data_new/pan_14essay_lm/')
-    PATH_LM.mkdir(exist_ok=True)
-
-    pan_data14 = PANData(year="14", train_split=["pan14_train_english-essays"],
-                         test_split=["pan14_test01_english-essays", "pan14_test02_english-essays"]
-                         # , known_as_list=True
-                         )
-
-    all_txt = pan_data14.get_train()['u_doc']
-    all_txt = all_txt.append(pan_data14.get_train()['k_doc'])
-
-    all_txt = all_txt.append(pan_data14.get_test()['u_doc'])
-    all_txt = all_txt.append(pan_data14.get_test()['k_doc'])
-
-    uniq_doc = pd.unique(all_txt.values.ravel('K'))
-
-    np.random.seed(42)
-    rdn_idx = np.random.permutation(len(uniq_doc))
-
-    uniq_doc = uniq_doc[rdn_idx]
-
-    with open(PATH_LM / 'train.txt', 'w', encoding="utf-8") as train_file:
-        for i in range(len(uniq_doc) - 50):
-            train_file.write(uniq_doc[i] + "\n")
-
-    with open(PATH_LM / 'test.txt', 'w', encoding="utf-8") as test_file:
-        for i in range(len(uniq_doc) - 50, len(uniq_doc)):
-            test_file.write(uniq_doc[i] + "\n")
-
-    print("ok")
-
-
-def convert_pan15_lm():
-    PATH_LM = Path('data_new/pan_15_lm/')
-    PATH_LM.mkdir(exist_ok=True)
-
-    pan_data15 = PANData(year="15", train_split=["pan15_train"], test_split=["pan15_test"])
-
-    all_txt = pan_data15.get_train()['u_doc']
-    all_txt = all_txt.append(pan_data15.get_train()['k_doc'])
-
-    all_txt = all_txt.append(pan_data15.get_test()['u_doc'])
-    all_txt = all_txt.append(pan_data15.get_test()['k_doc'])
-
-    uniq_doc = pd.unique(all_txt.values.ravel('K'))
-
-    np.random.seed(42)
-    rdn_idx = np.random.permutation(len(uniq_doc))
-
-    uniq_doc = uniq_doc[rdn_idx]
-
-    with open(PATH_LM / 'train.txt', 'w', encoding="utf-8") as train_file:
-        for i in range(len(uniq_doc) - 50):
-            train_file.write(uniq_doc[i] + "\n")
-
-    with open(PATH_LM / 'test.txt', 'w', encoding="utf-8") as test_file:
-        for i in range(len(uniq_doc) - 50, len(uniq_doc)):
-            test_file.write(uniq_doc[i] + "\n")
-
-    print("ok")
-
-
-
 def convert_pan13():
-    PATH_CLS = Path('data/pan_13_cls/')
+    PATH_CLS = Path('./data_new/pan_13_cls/')
     PATH_CLS.mkdir(exist_ok=True)
 
-    pan_data13 = PANData(year="13", train_split=["pan13_train"], test_split=["pan13_test01"], known_as_list=True)
+    pan_data13 = PANData(year="13", train_split=["pan13_train"], test_split=["pan13_test01"], known_as="str")
 
-    # pan_data13.get_train().to_csv(PATH_CLS / 'train13.csv', header=True, index=False)
-    # pan_data13.get_test().to_csv(PATH_CLS / 'test13_test01.csv', header=True, index=False)
+    # pan_data13.get_train().to_csv(PATH_CLS / 'train.csv', header=True, index=False)
+    # pan_data13.get_test().to_csv(PATH_CLS / 'test01.csv', header=True, index=False)
+    pan_data13.get_train().to_pickle(PATH_CLS / 'train.pickle')
+    pan_data13.get_test().to_pickle(PATH_CLS / 'test02.pickle')
 
-    pan_data13.get_train().to_pickle(PATH_CLS / 'train13.pickle')
-    pan_data13.get_test().to_pickle(PATH_CLS / 'test13_01.pickle')
-
-    pan_data13 = PANData(year="13", train_split=["pan13_train"], test_split=["pan13_test02"], known_as_list=True)
-
-    # pan_data13.get_test().to_csv(PATH_CLS / 'test13_test02.csv', header=True, index=False)
-    pan_data13.get_test().to_pickle(PATH_CLS / 'test13_02.pickle')
+    pan_data13 = PANData(year="13", train_split=["pan13_train"], test_split=["pan13_test02"], known_as="str")
+    # pan_data13.get_test().to_csv(PATH_CLS / 'test02.csv', header=True, index=False)
 
     print("ok")
 
 
 def convert_pan14_essay():
-    PATH_CLS = Path('data/pan_14e_cls/')
+    PATH_CLS = Path('data_new/pan_14e_cls/')
     PATH_CLS.mkdir(exist_ok=True)
 
     pan_data14 = PANData(year="14",
                          train_split=["pan14_train_english-essays"],
-                         test_split=["pan14_test02_english-essays"], known_as_list=True)
+                         test_split=["pan14_test02_english-essays"], known_as="list")
 
-    pan_data14.get_train().to_pickle(PATH_CLS / 'rawdf_train.pickle')
-    pan_data14.get_test().to_pickle(PATH_CLS / 'rawdf_test_02.pickle')
+    # pan_data14.get_train().to_csv(PATH_CLS / 'train_essays.csv', header=True, index=False)
+    # pan_data14.get_test().to_csv(PATH_CLS / 'test02_essays.csv', header=True, index=False)
+    pan_data14.get_train().to_pickle(PATH_CLS / 'train_essays.pickle')
+    pan_data14.get_test().to_pickle(PATH_CLS / 'test02_essays.pickle')
 
     pan_data14 = PANData(year="14",
                          train_split=["pan14_train_english-essays"],
-                         test_split=["pan14_test01_english-essays"], known_as_list=True)
+                         test_split=["pan14_test01_english-essays"], known_as="list")
 
-    pan_data14.get_test().to_pickle(PATH_CLS / 'rawdf_test_01.pickle')
+    # pan_data14.get_test().to_csv(PATH_CLS / 'test01_essays.csv', header=True, index=False)
+    pan_data14.get_test().to_pickle(PATH_CLS / 'test01_essays.pickle')
 
     print("ok")
 
 
 def convert_pan14_novel():
-    PATH_CLS = Path('data/pan_14n_cls/')
+    PATH_CLS = Path('data_new/pan_14n_cls/')
     PATH_CLS.mkdir(exist_ok=True)
 
     pan_data14 = PANData(year="14",
                          train_split=["pan14_train_english-novels"],
-                         test_split=["pan14_test02_english-novels"], known_as_list=True)
+                         test_split=["pan14_test02_english-novels"], known_as="str")
 
-    pan_data14.get_train().to_pickle(PATH_CLS / 'train14.pickle')
-    pan_data14.get_test().to_pickle(PATH_CLS / 'test14_02.pickle')
+    pan_data14.get_train().to_csv(PATH_CLS / 'train_novels.csv', header=True, index=False)
+    pan_data14.get_test().to_csv(PATH_CLS / 'test02_novels.csv', header=True, index=False)
 
     print("ok")
 
@@ -287,29 +184,18 @@ def convert_pan15():
 
     pan_data15 = PANData(year="15", train_split=["pan15_train"], test_split=["pan15_test"])
 
-    pan_data15.get_train().to_csv(PATH_CLS / "train.csv", index=False)
-    pan_data15.get_test().to_csv(PATH_CLS / "test.csv", index=False)
-
-    # pan_data15.get_train().to_pickle(PATH_CLS / 'train15.pickle')
-    # pan_data15.get_test().to_pickle(PATH_CLS / 'test15.pickle')
-
-    # with open(PATH_CLS / 'train15.txt', 'w', encoding="utf-8") as train_file:
-    #     for i in range(len(pan_data15.get_train())):
-    #         train_file.write(pan_data15.get_train()["label"][1] + "\n")
-    #
-    # with open(PATH_CLS / 'test15.txt', 'w', encoding="utf-8") as test_file:
-    #     for i in range(len(pan_data15.get_test())):
-    #         test_file.write(pan_data15.get_test()[i] + "\n")
+    # pan_data15.get_train().to_csv(PATH_CLS / "train.csv", index=False)
+    # pan_data15.get_test().to_csv(PATH_CLS / "test.csv", index=False)
+    pan_data15.get_train().to_pickle(PATH_CLS / 'train.pickle')
+    pan_data15.get_test().to_pickle(PATH_CLS / 'test.pickle')
 
     print("ok")
 
 
 if __name__ == "__main__":
-    # convert_pan14_essay()
-    # convert_pan14_novel()
+    os.chdir("./AV_with_DV/")
+    convert_pan13()
+    convert_pan14_essay()
+    convert_pan14_novel()
     convert_pan15()
-
-    # convert_pan14_essay_lm()
-    # convert_pan15_lm()
-    # convert_all_lm()
 
